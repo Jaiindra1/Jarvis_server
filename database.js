cf30a459d850ebd0dc51852a4ea91ebe9342305f
Jaiindra1 <jaiindra364@gmail.com>
2026-03-16 08:32:18 +0530

    fix database driver

--- database.js
+++ database.js
@@ -1,20 +1,16 @@
-const sqlite3 = require("sqlite3").verbose()
+const Database = require("better-sqlite3")
 
-const db = new sqlite3.Database("./jarvis.db")
+const db = new Database("jarvis.db")
 
-db.serialize(() => {
-
-    db.run(`
-        CREATE TABLE IF NOT EXISTS transactions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            source TEXT,
-            amount REAL,
-            type TEXT,
-            reference TEXT,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    `)
-
-})
+db.prepare(`
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    source TEXT,
+    amount REAL,
+    type TEXT,
+    reference TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+`).run()
 
 module.exports = db
